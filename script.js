328f04034067e807d820c1477de6e7f32d677c9f
lowscc <clarelowsc@gmail.com>
2024-11-04 08:12:27 +0000

    first commit with js

---
+++ script.js
@@ -0,0 +1,32 @@
+var age = 20    
+age= 65
+
+var username = "clare"
+var password = "hello"
+
+var isFemale = true
+var isLoggedIn = false
+
+var blog_posts = ["Today is a good day!", "How are you?", "It is sunny."]
+
+console.log(username)
+console.log(password)
+console.log(isFemale)
+console.log(isLoggedIn)
+console.log(blog_posts)
+
+if (isFemale == true) {
+    console.log("You are female")
+} else {
+    console.log("You are not female")
+}
+
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+
+function sayHello() {
+    alert("Hello everyone");
+}
+
